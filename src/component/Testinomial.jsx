254017f09578798a5ad4c7cd66efba8666ac405a
AYUSHMISHRA2003 <ayushm185@gmail.com>
2025-05-19 16:39:25 +0530

    testinomial updated

--- src/component/Testinomial.jsx
+++ src/component/Testinomial.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 const Testinomial = () => {
   const primaryColor = "#E47F97"; // Rose/pink
   const secondaryColor = "#5B2E67"; // Deep purple
+
   return (
     <section className="bg-gray-50 py-10 px-4">
       <div className="max-w-7xl mx-auto text-center">
@@ -14,15 +15,13 @@ const Testinomial = () => {
           Dr. Monika Pandey is caring, attentive, and truly committed to patient well-being. I felt supported every step of the way.
         </p>
 
-
-        <div className="shadow-lg rounded-lg overflow-hidden border border-gray-200">
+        <div className="shadow-lg rounded-lg overflow-hidden ">
           <iframe
-            src="https://cdn.trustindex.io/amp-widget.html#0a6234b4540a97560b86aa18478"
+            src="https://widgets.sociablekit.com/google-reviews/iframe/25558421"
+            frameBorder="0"
             width="100%"
-            height="268"
-            style={{ border: 'none', overflow: 'hidden' }}
-            sandbox="allow-scripts allow-same-origin"
-            loading="lazy"
+            height="400"
+       
             title="Customer Testimonials"
           ></iframe>
         </div>
